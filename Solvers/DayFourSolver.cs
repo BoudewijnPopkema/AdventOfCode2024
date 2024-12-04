69643359c0a2bd6cc79f3791b2faa47b64fb8ee3
Boudewijn Popkema <boudewijn@popkema.com>
2024-12-04 22:27:20 +0100

    solved day four but ugly

--- Solvers/DayFourSolver.cs
+++ Solvers/DayFourSolver.cs
@@ -1,48 +1,90 @@
 ﻿using System.Text.RegularExpressions;
 
 namespace AdventOfCode2024.Solvers;
+
 public partial class DayFourSolver : DaySolver
 {
     public override int Day => 4;
-    private string[]? _horizontalStrings;
-    private string[]? _verticalStrings;
-    private string[]? _diagonalAscendingStrings;
-    private string[]? _diagonalDescendingStrings;
+    private char[][]? _grid;
+    private int _size;
+
     public override void LoadInputData(string[] input)
     {
-        var size = input.Length;
-        _horizontalStrings = input;
-        _verticalStrings = new string[size];
-        _diagonalAscendingStrings = new string[size];
-        _diagonalDescendingStrings = new string[size];
-
-        for (var i = 0; i < size; i++)
-        {
-            _verticalStrings[i] = new string(input.Select(s => s[i]).ToArray());
-        }
-
-        // Get all diagonal ascending lines, starting at top left
-        for (var line = 0; line < size; line++)
-        {
-            for (var pos = 0; pos <= line + size; pos++)
-            {
-                var x = Math.Min(size, pos);
-                var y = line - pos;
-                _diagonalAscendingStrings[line] += input[x][y];
-                _diagonalDescendingStrings[line] += input[size - x - 1][y];
-            }
-        }
+        _grid = input.Select(x => x.ToCharArray()).ToArray();
+        _size = _grid[0].Length;
     }
+
     public override string SolvePartOne()
     {
-        var strings = _diagonalAscendingStrings.Concat(_diagonalDescendingStrings).Concat(_verticalStrings).Concat(_horizontalStrings);
-        return XmasRegex().Matches(string.Join(',',strings)).Count.ToString();
-    }
-    public override string SolvePartTwo()
-    {
-        throw new NotImplementedException();
+        var diagonals = _size * 2 - 1;
+        var horizontalStrings = new string[_size];
+        var verticalStrings = new string[_size];
+        for (var x = 0; x < _size; x++)
+        {
+            for (var y = 0; y < _size; y++)
+            {
+                verticalStrings[x] += _grid[x][y];
+                horizontalStrings[y] += _grid[x][y];
+            }
+        }
+
+        var diagonalAscendingStrings = new string[diagonals];
+        var diagonalDescendingStrings = new string[diagonals];
+
+        // Get all diagonal ascending lines, starting at top left
+        for (var line = 0; line < diagonals; line++)
+        {
+            var startX = line < _size ? 0 : line - _size + 1;
+            var startY = line < _size ? line : _size - 1;
+            for (var pos = 0; pos <= line; pos++)
+            {
+                var x = startX + pos;
+                var y = startY - pos;
+
+                if (y < 0 || x >= _size) break;
+
+                diagonalAscendingStrings[line] += _grid[x][y];
+                diagonalDescendingStrings[line] += _grid[_size - x - 1][y];
+            }
+        }
+
+        var strings = diagonalAscendingStrings.Concat(diagonalDescendingStrings).Concat(verticalStrings).Concat(horizontalStrings);
+        var joinedStrings = string.Join(' ', strings);
+        return (XmasRegex().Matches(joinedStrings).Count + SamxRegex().Matches(joinedStrings).Count).ToString();
     }
 
-    [GeneratedRegex("XMAS|SAMX")]
+    public override string SolvePartTwo()
+    {
+        var matches = 0;
+        for (var x = 1; x < _size - 1; x++)
+        {
+            for (var y = 1; y < _size - 1; y++)
+            {
+                if (GridMatchesXMAS(x, y))
+                    matches++;
+            }
+        }
+
+        return matches.ToString();
+    }
+
+    private bool GridMatchesXMAS(int x, int y)
+    {
+        var tl = _grid![x - 1][y + 1];
+        var tr = _grid[x + 1][y + 1];
+        var bl = _grid[x - 1][y - 1];
+        var br = _grid[x + 1][y - 1];
+
+        return _grid[x][y] == 'A'
+               && (tl == 'S' && tr == 'S' && bl == 'M' && br == 'M'
+                   || tl == 'S' && tr == 'M' && bl == 'S' && br == 'M'
+                   || tl == 'M' && tr == 'M' && bl == 'S' && br == 'S'
+                   || tl == 'M' && tr == 'S' && bl == 'M' && br == 'S');
+    }
+
+    [GeneratedRegex("XMAS")]
     private static partial Regex XmasRegex();
+
+    [GeneratedRegex("SAMX")]
+    private static partial Regex SamxRegex();
 }
