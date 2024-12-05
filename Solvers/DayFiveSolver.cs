c5f241c1aa44731bd54c30d382c0195b5ba62deb
Boudewijn Popkema <boudewijn@popkema.com>
2024-12-05 21:27:17 +0100

    day 5 part two

--- Solvers/DayFiveSolver.cs
+++ Solvers/DayFiveSolver.cs
@@ -29,18 +29,41 @@ public class DayFiveSolver : DaySolver
 
     public override string SolvePartTwo()
     {
-        throw new NotImplementedException();
+        return _updates
+            .Where(u => !IsCorrectlyOrdered(u))
+            .Select(OrderByRules)
+            .Sum(u => u[u.Count / 2])
+            .ToString();
     }
+    
+    private List<(int,int)> GetRulesForUpdate(List<int> update) => _rules.Where(r => update.Any(x => x == r.Item1) && update.Any(x => x == r.Item2)).ToList();
 
     private bool IsCorrectlyOrdered(List<int> update)
     {
-        var relevantRules = _rules.Where(r => update.Any(x => x == r.Item1) && update.Any(x => x == r.Item2)).ToList();
-
-        return !(from rule in relevantRules
+        return !(from rule in GetRulesForUpdate(update)
                  let indexItem1 = update.IndexOf(rule.Item1)
                  let indexItem2 = update.IndexOf(rule.Item2)
                  where indexItem1 > indexItem2
                  select 1)
             .Any();
+    }
+
+    private List<int> OrderByRules(List<int> update)
+    {
+        var rules = GetRulesForUpdate(update).OrderBy(x => x.Item1);
+        foreach (var rule in rules)
+        {
+            var indexItem1 = update.IndexOf(rule.Item1);
+            var indexItem2 = update.IndexOf(rule.Item2);
+
+            if (indexItem1 < indexItem2)
+                continue;
+
+            var item1 = update.First(x => x == rule.Item1);
+            update.Remove(item1);
+            update.Insert(indexItem2, item1);
+        }
+        
+        return update;
     }
 }
